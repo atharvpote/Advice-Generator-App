8484620d1189977e3d7903b6fcf45b964c8f8176
Atharv Pote <atharvpote@gmail.com>
2022-04-27 21:27:33 +0530

    Completed.

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+const adviceID = document.querySelector("#advice-id");
+const adviceQuote = document.querySelector("#advice-quote");
+const newAdviceBtn = document.querySelector("#new-advice");
 
+window.addEventListener("load", getQuote);
+newAdviceBtn.addEventListener("click", getQuote);
+
+async function getQuote() {
+  let response = await fetch("https://api.adviceslip.com/advice");
+  response = await response.json();
+
+  const { id, advice } = response.slip;
+
+  adviceID.innerText = id;
+  adviceQuote.innerText = `"${advice}"`;
+}
